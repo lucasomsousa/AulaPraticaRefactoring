a7d7d1e6d821ac1412ac0c1c6f83e104f349f775
Replit user <replituser@example.com>
2021-03-12 23:08:01 +0000

    Commit 6

--- Rental.java
+++ Rental.java
@@ -35,4 +35,9 @@ public class Rental {
       }
     return(thisAmount);
    }
+
+   public int getFrequentRenterPoints(){
+     if ((this.getMovie().getPriceCode() == Movie.NEW_RELEASE)      && this.getDaysRented() > 1) return 2;
+     else return 1;
+   }
 }
